c963b4e7c1a4baca8af8c3d046a6234e11c273bb
blackPeanut <daniel@galt.hk>
2016-01-27 12:00:03 +0100

    Remove unused/old files and set correct paths in index.kit

--- js/min/menuData-min.js
+++
@@ -1,1 +0,0 @@
-var menuData={authors:[{route:"jacklondon",authorName:"JackLondon"},{route:"frankbaum",authorName:"Frank Baum"},{route:"lewiscarroll",authorName:"Lewis Carrrol"},{route:"index","class":"logo",authorName:"LogoTest"},{route:"julesverne",authorName:"Jules Verne"},{route:"hgwells",authorName:"H.G.Wells"},{route:"hermanmelville",authorName:"Herman Melville"}]};
