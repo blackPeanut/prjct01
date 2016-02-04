1bc87b7a27e3eb9d29270b16f99dc3d986b6b26d
blackPeanut <daniel@galt.hk>
2016-02-04 18:13:01 +0100

    Rewrited: Gulp inject Added: Gulp inline minifyhtml compress
    Url's works correctj

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,112 @@
-var gulp = require('gulp');
-var path = require('path');
-
-var inject = require('gulp-inject');
-var less = require('gulp-less');
-var cssnano = require('gulp-cssnano');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-
+var 
+  gulp = require('gulp'),
+  path = require('path'),
+  inject = require('gulp-inject'),
+  less = require('gulp-less'),
+  cssnano = require('gulp-cssnano'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify'),
+  kit = require('gulp-kit'),
+  inline = require('gulp-inline'),
+  series = require('stream-series'),
+  htmlmin = require('gulp-htmlmin'),
+  gzip = require('gulp-gzip');
 /*
-var LessPluginCleanCSS = require('less-plugin-clean-css');
-var cleancss = new LessPluginCleanCSS({ advanced: true });
+var runSequence = require('run-sequence');
+var del = require('del');
+var fs = require('fs');
 */
 
-//gulp.task('default', ['less', 'mincss']); 
+gulp.task('default', ['minifyhtml']);
+
+/*
+gulp.task('build', function(callback) {
+  runSequence('less',
+              ['head-scripts', 'body-scripts'],
+              'kit',
+              'inject',
+              'inline',
+              'minifyhtml',
+              callback);
+});
+*/
 
 gulp.task('less', function () {
   return gulp.src('./css/main.less')
-    .pipe(less({
-      paths: ['.']
-      //plugins: [ cleancss ]
-    }))
+    .pipe(less({relativeUrls: true}))
     .pipe(gulp.dest('./css'))
-    //good flow? 
-    .pipe(rename('mainmin.css'))
-    .pipe(gulp.dest('./css/min'))
     .pipe(cssnano())
+    .pipe(rename('main.min.css'))
     .pipe(gulp.dest('./css/min'))
 });
 
-gulp.task('scripts', function () {
-  return gulp.src(['./js/data.js', './js/*.js'])
-  .pipe(concat('all.js'))
-  .pipe(gulp.dest('./js/all/'))
+gulp.task('scripts', ['less']);
+gulp.task('head-scripts', function () {
+  return gulp.src(['./js/cacheMainBackground.js', './js/plugins.js'])
+    .pipe(concat('headScriptsAll.js'))
+    //.pipe(gulp.dest('./js/all')) -- no need for this action, but is it good practice?
+    .pipe(rename('headScriptsAllMin.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./js/all'))
 });
 
-gulp.task('index', function () {
-  var target = gulp.src('./index.html');
-  var sources = gulp.src(['./css/*.css'], {read: false});
-
-  return target.pipe(inject(sources))
-    .pipe(gulp.dest('./'));
+gulp.task('body-scripts', function () {
+  return gulp.src(['./js/data.js', './js/main.js'])
+    .pipe(concat('bodyScriptsAll.js'))
+    //.pipe(gulp.dest('./js/all')) -- no need for this action, but is it good practice?
+    .pipe(rename('bodyScriptsAllMin.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./js/all'))
 });
+
+gulp.task('kit', ['scripts'], function () {
+  return gulp.src('./index.kit')
+    .pipe(kit({compilePartials: true}))
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('inject', ['kit'], function () {
+  var options = {
+    relative: true 
+  };
+
+  return gulp.src('./index.html')
+    .pipe(inject(gulp.src(['./css/min/normalize.min.css', './css/min/main.min.css'], {read: false}), options))
+    .pipe(inject(gulp.src('./js/all/headScriptsAllMin.js', {read: false}), {name: 'head', relative: true}))
+    .pipe(inject(gulp.src(['./js/vendor/director.js', './js/vendor/hogan.js', './js/all/bodyScriptsAllMin.js'], {read: false}), options))
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('inline', ['inject'], function () {
+  var options = {
+    base: './', 
+  }
+  return gulp.src('./index.html')
+    .pipe(inline(options))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('minifyhtml', ['inline'], function() {
+  var options = {
+    collapseWhitespace: true,
+    removeComments: true,
+    removeTagWhitespace: true,
+    removeAttributeQuotes: true,
+    useShortDoctype: true,
+    removeScriptTypeAttributes: true,
+    minifyJS: true
+  }
+
+  return gulp.src('./index.html')
+    .pipe(htmlmin(options))
+    .pipe(gulp.dest('./'))
+});
+
+/*
+gulp.task('compress', function() {
+    gulp.src('./js/all/bodyScriptsAllMin.js')//index.html')
+	.pipe(gzip())
+	.pipe(gulp.dest('./'));
+});
+*/
